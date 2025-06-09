b3b54d7e4525fc69757c5238ece552c43aa56b54
oculusK <kay5990@naver.com>
2025-06-10 06:32:17 +0900

    refactor : 게이트 웨이 수정 : image 관련

--- src/main/java/com/nhnacademy/gatewayservice/config/GatewayConfig.java
+++ src/main/java/com/nhnacademy/gatewayservice/config/GatewayConfig.java
@@ -112,6 +112,10 @@ public class GatewayConfig {
                         .path("/api/v1/floors/**")
                         .uri("lb://image-service"))
 
+                .route("image-service-places", r -> r
+                        .path("/api/v1/places/**")
+                        .uri("lb://image-service"))
+
                 .route("iot-service-images", r -> r
                         .path("/images/**")
                         .uri("lb://image-service"))
